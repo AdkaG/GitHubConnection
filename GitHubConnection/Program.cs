6e503373b8497b12b94ed590d194a19f0cd621e5
AdkaG <adriana.grzybowska1@gmail.com>
2015-12-07 11:09:54 +0100

    New branch test

--- GitHubConnection/Program.cs
+++ GitHubConnection/Program.cs
@@ -11,6 +11,7 @@ namespace GitHubConnection
         static void Main(string[] args)
         {
             Console.WriteLine("First change");
+            Console.WriteLine("Adkas change");
         }
     }
 }
